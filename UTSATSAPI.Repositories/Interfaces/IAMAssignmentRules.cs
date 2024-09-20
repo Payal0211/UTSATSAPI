f6dca06e92e47d3c9e70892e350433ada88bc3f8
Himani <himani.s@uplers.com>
2024-09-20 20:13:22 +0530

    delete unneccsary files

--- UTSATSAPI.Repositories/Interfaces/IAMAssignmentRules.cs
+++
@@ -1,9 +0,0 @@
-﻿using UTSATSAPI.Models.ComplexTypes;
-
-namespace UTSATSAPI.Repositories.Interfaces
-{
-    public interface IAMAssignmentRules
-    {
-        Task<List<sproc_UTS_GetAMAssignmentRules_Result>> GetAMAssignmentRules(string paramasString);
-    }
-}
--- UTSATSAPI.Repositories/Interfaces/IHRInterviewerDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UTSATSAPI.Repositories.Interfaces
-{
-    using UTSATSAPI.Models.ViewModel;
-
-    public interface IHRInterviewerDetail
-    {
-        List<SalesHiringRequestInterviewerDetailViewModel> GetHRInterviewerDetails(long HiringRequestID);
-    }
-}
